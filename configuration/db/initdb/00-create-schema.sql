23ba619528ab3a2dd30c9b30f85e24044e031776
igortitoneli <igor.titoneli@estudante.ufjf.br>
2025-02-23 22:02:42 -0300

    feat: places and reviews

--- configuration/db/initdb/00-create-schema.sql
+++ configuration/db/initdb/00-create-schema.sql
@@ -6,8 +6,36 @@ CREATE TABLE `customers` (
   `email` varchar(100) NOT NULL,
   `password` varchar(60) NOT NULL,
   `phone` varchar(20) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT NOW(),
-  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `places` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `latitude` float NOT NULL,
+  `longitude` float NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE `reviews` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `customers_id` int NOT NULL,
+  `places_id` int NOT NULL,
+  `message` text NOT NULL,
+  `stars` float NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `customers_id_fk` FOREIGN KEY (`customers_id`) REFERENCES `customers` (`id`),
+  CONSTRAINT `places_id_fk` FOREIGN KEY (`places_id`) REFERENCES `places` (`id`),
+  CONSTRAINT `check_stars` CHECK (`stars` BETWEEN 1 AND 5)
 );
